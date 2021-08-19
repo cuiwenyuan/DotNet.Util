fb4c834a028f4e882275d7414a603d250c76fac6
Troy Cui <troy@cuiwenyuan.com>
2021-08-19 20:21:07 +0800

    DotNet.Util新增Pool，源自大石头的新生命NewLife.Core，做了依赖的去除，直接可以复制Pool目录添加到你自己的项目中使用。

---
+++ src/DotNet.Util/Pool/IPool.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DotNet.Util
+{
+    /// <summary>对象池接口</summary>
+    /// <remarks>
+    /// 文档 https://www.yuque.com/smartstone/nx/object_pool
+    /// </remarks>
+    /// <typeparam name="T"></typeparam>
+    public interface IPool<T> where T : class
+    {
+        /// <summary>对象池大小</summary>
+        Int32 Max { get; set; }
+
+        /// <summary>获取</summary>
+        /// <returns></returns>
+        T Get();
+
+        /// <summary>归还</summary>
+        /// <param name="value"></param>
+        Boolean Put(T value);
+
+        /// <summary>清空</summary>
+        Int32 Clear();
+    }
+}
