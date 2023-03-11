04a7f72404391a6a77fb448dd8dd0eb8a73c6f60
Troy Cui <troy@cuiwenyuan.com>
2023-03-11 16:40:12 +0800

    删除BaseResult中的ResultValue

--- src/DotNet.Util/Model/BaseResult.cs
+++ src/DotNet.Util/Model/BaseResult.cs
@@ -38,12 +38,6 @@ namespace DotNet.Util
         public string Result = "";
 
         /// <summary>
-        /// 返回值
-        /// </summary>
-        [Obsolete("Please use Result from 2022-08-18")]
-        public string ResultValue => Result;
-
-        /// <summary>
         /// 返回状态代码
         /// </summary>
         public string StatusCode = "UnknownError";
